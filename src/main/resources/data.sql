3d7c1f72d37cd696f2afcda33f155aa8a38ea183
Александра Галеева <galeevaalexandera@yandex.ru>
2026-07-29 12:36:30 +0300

    Fix data.sql initialization

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,21 +1,15 @@
-INSERT INTO mpa (id, name)
-VALUES
-(1, 'G'),
-(2, 'PG'),
-(3, 'PG-13'),
-(4, 'R'),
-(5, 'NC-17');
+MERGE INTO mpa KEY(id) VALUES (1, 'G');
+MERGE INTO mpa KEY(id) VALUES (2, 'PG');
+MERGE INTO mpa KEY(id) VALUES (3, 'PG-13');
+MERGE INTO mpa KEY(id) VALUES (4, 'R');
+MERGE INTO mpa KEY(id) VALUES (5, 'NC-17');
 
-INSERT INTO genres (id, name)
-VALUES
-(1, 'Комедия'),
-(2, 'Драма'),
-(3, 'Мультфильм'),
-(4, 'Триллер'),
-(5, 'Документальный'),
-(6, 'Боевик');
+MERGE INTO genres KEY(id) VALUES (1, 'Комедия');
+MERGE INTO genres KEY(id) VALUES (2, 'Драма');
+MERGE INTO genres KEY(id) VALUES (3, 'Мультфильм');
+MERGE INTO genres KEY(id) VALUES (4, 'Триллер');
+MERGE INTO genres KEY(id) VALUES (5, 'Документальный');
+MERGE INTO genres KEY(id) VALUES (6, 'Боевик');
 
-INSERT INTO friend_status (id, name)
-VALUES
-(1, 'UNCONFIRMED'),
-(2, 'CONFIRMED');
+MERGE INTO friend_status KEY(id) VALUES (1, 'UNCONFIRMED');
+MERGE INTO friend_status KEY(id) VALUES (2, 'CONFIRMED');
