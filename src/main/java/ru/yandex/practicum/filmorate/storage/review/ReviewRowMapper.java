30463bffd09b877574df3675898bf48891305c1d
AntonKellerman <kellermankupriyanov@yandex.ru>
2026-08-21 21:11:31 +0300

    HotFix 1.0

--- src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewRowMapper.java
@@ -16,7 +16,7 @@ public class ReviewRowMapper implements RowMapper<Review> {
 
         review.setId(rs.getLong("id"));
         review.setContent(rs.getString("content"));
-        review.setPositive(rs.getBoolean("is_positive"));
+        review.setIsPositive(rs.getBoolean("is_positive"));
         review.setUserId(rs.getLong("user_id"));
         review.setFilmId(rs.getLong("film_id"));
         review.setUseful(rs.getLong("rating"));
