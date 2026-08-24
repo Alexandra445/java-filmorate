7814818ce953c58549077d2758e3963144445f1e
AntonKellerman <kellermankupriyanov@yandex.ru>
2026-08-24 19:12:47 +0300

    Логирование в классе RecommendationService.

--- src/main/java/ru/yandex/practicum/filmorate/service/RecommendationService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/RecommendationService.java
@@ -1,12 +1,14 @@
 package ru.yandex.practicum.filmorate.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.recommendation.RecommendationStorage;
 
 import java.util.Collection;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class RecommendationService {
@@ -14,6 +16,7 @@ public class RecommendationService {
     private final RecommendationStorage recommendationStorage;
 
     public Collection<Film> findRecommendationsForUser(Long userId) {
+        log.debug("Поиск рекомендаций по фильмам для пользователя с id {}", userId);
         return recommendationStorage.getRecommendedFilms(userId);
     }
 }
