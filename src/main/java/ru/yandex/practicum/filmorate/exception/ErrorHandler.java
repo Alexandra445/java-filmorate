e496814e0e8e36f4d2eb426b974ba2b38a8e9533
Mikhail <bobkowmisch@yandex.ru>
2026-08-25 18:53:14 +0300

    fix style

--- src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ErrorHandler.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.exception;
 
 
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
