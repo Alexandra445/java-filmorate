718634e3ff50de68d6240875dfcfd5fc693f5f5f
AntonKellerman <kellermankupriyanov@yandex.ru>
2026-08-27 18:41:17 +0300

    Fix recommendation system

--- src/main/java/ru/yandex/practicum/filmorate/storage/recommendation/RecommendationDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/recommendation/RecommendationDbStorage.java
@@ -21,32 +21,26 @@ public class RecommendationDbStorage implements RecommendationStorage {
 
     private static final String FIND_RECOMMEND_FILMS = """
             WITH similar_user AS (
-                            SELECT other_likes.user_id AS similar_id,
-                                   COUNT(*) AS cnt
-                            FROM (
-                                SELECT film_id
-                                FROM likes
-                                WHERE user_id = ?
-                            ) AS my_likes
-                            JOIN likes AS other_likes ON other_likes.film_id = my_likes.film_id
-                            WHERE other_likes.user_id != ?
-                            GROUP BY other_likes.user_id
-                            ORDER BY cnt DESC
-                            LIMIT 1
-                        )
-                        SELECT f.*
-                        FROM films AS f
-                        WHERE f.id IN (
-                            SELECT l.film_id
-                            FROM likes AS l
-                            JOIN similar_user AS su ON su.similar_id = l.user_id
-                              AND NOT EXISTS (
-                                  SELECT 1
-                                  FROM likes my_likes
-                                  WHERE my_likes.user_id = ?
-                                    AND my_likes.film_id = l.film_id
-                              )
-                        )
+                SELECT other_likes.user_id AS similar_id,
+                       COUNT(*) AS cnt
+                FROM (
+                    SELECT film_id
+                    FROM likes
+                    WHERE user_id = ?  -- ID целевого пользователя
+                ) my_likes
+                JOIN likes other_likes ON other_likes.film_id = my_likes.film_id
+                WHERE other_likes.user_id != ?
+                GROUP BY other_likes.user_id
+                ORDER BY cnt DESC
+                LIMIT 1
+            )
+            SELECT f.*
+            FROM films f
+            JOIN likes l ON l.film_id = f.id
+            JOIN similar_user su ON su.similar_id = l.user_id
+            LEFT JOIN likes my_likes ON my_likes.film_id = l.film_id
+                                     AND my_likes.user_id = ?  -- ID целевого пользователя
+            WHERE my_likes.film_id IS NULL;
             """;
 
     public RecommendationDbStorage(JdbcTemplate jdbcTemplate,
